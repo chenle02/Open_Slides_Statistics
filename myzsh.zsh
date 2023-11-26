069eef0b4083023ad9a52c8cb4bb914abed6ad94
chenle02 <chenle02@gmail.com>
2023-11-26 00:27:59 -0500

    Add topics

--- myzsh.zsh
+++ myzsh.zsh
@@ -1,2 +1,9 @@
 
-alias description="gh repo edit --description 'Statistiscs: Open Slides' --homepage 'https://zenodo.org/records/10206720'"
+alias description="gh repo edit \
+  --description 'Statistiscs: Open Slides' \
+  --homepage 'https://doi.org/10.5281/zenodo.10206720'\
+  --add-topic 'teaching' \
+  --add-topic 'slides' \
+  --add-topic 'statistics' \
+  --add-topic 'latex' \
+  "
